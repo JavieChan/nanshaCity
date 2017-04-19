c8c38aa85102a067cb59dc53a97240e68c2ac420
JavieChan <javie0302@live.cn>
2017-04-19 17:33:04 +0800

    add 兑换码

---
+++ mp/lib/redeemcode/redeemcode.js
@@ -0,0 +1,91 @@
+
+$(function(){
+    // 日期
+    $("#expired").datepicker({
+        dateFormat: 'yy-mm-dd',
+        onClose: function(selectedDate) {
+            vm.expired = selectedDate;
+        }
+    });
+    // 复制兑换码
+    var clipboard = new Clipboard('#clipboard');
+    clipboard.on('success', function(){
+        alert("本页兑换码已复制到剪贴板");
+    });
+    clipboard.on('error', function(){
+        alert("[复制失败]");
+    });
+});
+
+function GetDateStr(AddDayCount) {
+    var dd = new Date();
+    dd.setDate(dd.getDate()+AddDayCount);   //获取AddDayCount天后的日期
+    var y = dd.getFullYear();
+    var m = dd.getMonth()+1;     //获取当前月份的日期
+    var d = dd.getDate();
+    if(m<9) m="0"+m;
+    return y+"-"+m+"-"+d;
+}
+
+var renderList = {
+    template: '<tr><td>{{ code.code }}</td><td>{{ code.hours }}小时</td><td>至{{ code.expired }}</td></tr>',
+    props: ['code']
+};
+
+var ree = new redeemCode();
+var vm = new Vue({
+    el: '#redeemcode',
+    data: {
+        hours: 1,
+        count: 1,
+        expired: GetDateStr(1),
+        codeList: [],
+        currentPage: 0,
+        totalPage: 0,
+        jumpPage: '',
+        serial: '',
+        onePageCodes: ''
+    },
+    components: {
+        'render-list': renderList
+    },
+    computed: {
+        totalExpired: function(){
+            return this.expired+" 23:59:59";
+        },
+        exportCodes: function(){
+            var url = (this.serial == '') ? 'javascript:;' : 'http://10.20.1.34:8087/redeemcode/codes.xls?serial='+this.serial;
+            return url;
+        }
+    },
+    methods: {
+        generateCodes: function(){
+            var self = this;
+            if(self.count>200){
+                alert("兑换码数量超出上限(最多200个)");
+                return false;
+            }
+            ree.createSerial(self.count, self.hours, self.totalExpired, function(data){
+                self.serial= data.serial;
+                self.pageChange(1);
+                self.jumpPage = '';
+            });
+        },
+        pageChange: function(page){
+            var self = this;
+            ree.generateCodes(self.serial, page, function(data){
+                self.renderClipboard(data.codes);
+                self.currentPage = data.current_page;
+                self.totalPage = data.total_pages;
+                self.codeList = data.codes;
+            });
+        },
+        renderClipboard: function(codes){
+            for(var i=0, h='', len=codes.length; i<len; i++){
+                var code = codes[i];
+                h+=code.code + '\t' + code.hours + '小时\t有效期至' + code.expired + '\r\n';
+            }
+            this.onePageCodes = h;
+        }
+    }
+});
---
+++ mp/lib/request/http.js
@@ -0,0 +1,40 @@
+function Request(){
+    this.protocol= "http",                // 请求协议
+    this.host= "10.20.1.34",       // 请求域名或IP
+    this.port= "8087",                      // 请求端口
+    this.version= "v0"                   // API版本号
+}
+Request.prototype._send = function(type, pathname, params, success, fail){
+    var url = this.protocol+'://'+this.host+':'+this.port+pathname;
+
+    $.ajax({
+        method: type,
+        url: url,
+        dataType: "json",
+        data: params,
+    //    xhrFields:
+    //      withCredentials: true
+    //    crossDomain: true
+    }).done(function(res){
+        console.log(res);
+        if(parseInt(res.code) == 200){
+            if($.isFunction(success)){
+                success(res)
+            }
+        }else{
+            if($.isFunction(fail)){
+                fail(res)
+            }else{
+                alert(res.message);
+            }
+        }
+    }).fail(function(err){
+        alert("[请求失败]系统繁忙")
+    });
+};
+Request.prototype._post = function(pathname, params, success, fail){
+    this._send("post", pathname, params, success, fail);
+};
+Request.prototype._get = function(pathname, params, success, fail){
+    this._send("get", pathname, params, success, fail);
+};
