ce64a50967c1d11e979a66cedc58832d7d52b436
JavieChan <javie0302@live.cn>
2017-05-04 11:27:31 +0800

    modify code

--- mp/lib/wechat/wechat-http.js
+++ mp/lib/wechat/wechat-http.js
@@ -3,13 +3,62 @@ function wechatRequest(){
     Request.call(this);
 }
 wechatRequest.prototype = new Request();
-wechatRequest.prototype.createSerial = function(count, hours, expired, callback){
+wechatRequest.prototype.getWxpromote = function(callback){
+    this._get('/wxpromote', '', function(data){
+        callback(data);
+    });
+};
+wechatRequest.prototype.pustWxpromote = function(wechatId, nickname, account, password, uploadImg, callback){
     var params = {
-        count: count,
-        hours: hours,
-        expired: expired
+        wechat_id: wechatId,
+        nickname: nickname,
+        account: account,
+        password: password,
+        image_url: uploadImg
     };
-    this._post('/redeemcode/creation', JSON.stringify(params), function(data){
+    this._post('/wxpromote', JSON.stringify(params), function(data){
+        callback(data);
+    });
+};
+wechatRequest.prototype.putWxpromote = function(id, wechatId, nickname, account, password, uploadImg, callback){
+    var params = {
+        id: id,
+        wechat_id: wechatId,
+        nickname: nickname,
+        account: account,
+        password: password,
+        image_url: uploadImg
+    };
+    this._put('/wxpromote', JSON.stringify(params), function(data){
+        callback(data);
+    });
+};
+wechatRequest.prototype.deleteWxpromote = function(wechatId, callback){
+    var params = {
+        wechat_id: wechatId
+    };
+    this._delete('/wxpromote', JSON.stringify(params), function(data){
+        callback(data);
+    });
+};
+wechatRequest.prototype.deleteWxpromotes = function(wechatIdList, callback){
+    var params = {
+        wechat_id_list: wechatIdList
+    };
+    this._delete('/wxpromote/bulk-delete', JSON.stringify(params), function(data){
+        callback(data);
+    });
+};
+wechatRequest.prototype.getUsingWxpromote = function(callback){
+    this._get('/wxpromote/promoting', '', function(data){
+        callback(data);
+    });
+};
+wechatRequest.prototype.usingWxpromote = function(wechatId, callback){
+    var params = {
+        wechat_id: wechatId
+    };
+    this._post('/wxpromote/promoting', JSON.stringify(params), function(data){
         callback(data);
     });
 };
